83da8e17f8b7aca995fdeabe93e14e22afead432
riddhi <test01.rentechdigital@gmail.com>
2024-03-15 09:13:37 +0530

    drf

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const auth = require("./routes/auth");
 const list = require("./routes/list");
 app.use(express.json());
 app.use(cors());
+const multer = require('multer');
+const User = require("./models/user")
 
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
+
+
+
+app.get('/userall', async (req, res) => {
+  res.send('all user get..55');
+});
 
 app.get("/", (req, res) => {
   app.use(express.static(path.resolve(__dirname, "frontend", "build")));
